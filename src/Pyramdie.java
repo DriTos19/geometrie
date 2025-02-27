42e575f7faaff45fa5f613206bbf51c9dfae7a1d
drini <dritos19@htl-shkoder.com>
2025-02-27 08:24:50 +0100

    Konstruktor

--- src/Pyramdie.java
+++ src/Pyramdie.java
@@ -1,10 +1,6 @@
 public abstract class Pyramdie {
     private int hohe;
-    public Pyramdie(int hohe) {
-        this.hohe = hohe;
-    }
+    public Pyramdie(int hohe) {this.hohe = hohe;}
     public abstract float calc_volumen();
-    public int getHohe() {
-        return hohe;
-    }
+    public int getHohe() {return hohe;}
 }
